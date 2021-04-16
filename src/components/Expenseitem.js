ee322dfc771cf3de927a40b27282a8c266542c2a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-16 17:33:30 -0400

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,6 +1,9 @@
 //A component in react is just a function
+//You always create a component, then export it
+//then import in the file you want to use it so you can use it
+//like an html element
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    return <h2>Expense Item!</h2>
 }
 
 
