30d045a47e0b903d5c1554dafeb1906a8d471219
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-17 11:51:05 -0400

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -6,14 +6,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
       <div>March 28</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{expenseTitle}</h2>
       </div>
       <div className="expense-item__price">
-        <h2>Car Insurance</h2>
+        <h2>294.67</h2>
       </div>
     </div>
   );
