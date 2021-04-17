aab4d3952a3fed742e1fd39349a6cc37193610a1
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-17 11:34:24 -0400

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -19,16 +19,5 @@ function ExpenseItem() {
   );
 }
 
-// function ExpenseItem() {
-//   return (
-//     <div className="expense-item">
-//       <div>March 28 2021</div>
-//       <div className="expense-item__description">
-//         <h2>Title</h2>
-//       <div className="expense-item__price"/>
-//       <div>Amount</div>
-//     </div>
-//   );
-// }
 
 export default ExpenseItem;
