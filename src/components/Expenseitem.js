f74171e641444ee1ae235a3075a6fb78475ce1c1
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-17 11:32:19 -0400

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -3,18 +3,32 @@
 //then import in the file you want to use it so you can use it
 //like an html element
 //main thing is to have one root element, so one <div></div> wrapped
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
     <div className="expense-item">
-      <div>March 28 2021</div>
+      <div>March 28</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-      <div className="expense-item__price"/>
-      <div>Amount</div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">
+        <h2>Car Insurance</h2>
+      </div>
     </div>
   );
 }
+
+// function ExpenseItem() {
+//   return (
+//     <div className="expense-item">
+//       <div>March 28 2021</div>
+//       <div className="expense-item__description">
+//         <h2>Title</h2>
+//       <div className="expense-item__price"/>
+//       <div>Amount</div>
+//     </div>
+//   );
+// }
 
 export default ExpenseItem;
