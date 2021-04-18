9a5e8a22cd1105eb91ec1790eb60518d4d7d57eb
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-18 15:10:52 -0400

    Update Expenses.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,17 +7,17 @@ function Expenses(props) {}=>{
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}></ExpenseItem>
       <ExpenseItem
-        title={prop.expenses[1].title}
-        amount={prop.expenses[1].amount}
-        date={prop.expenses[1].date}></ExpenseItem>
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}></ExpenseItem>
       <ExpenseItem
-        title={prop.expenses[2].title}
-        amount={prop.expenses[2].amount}
-        date={prop.expenses[2].date}></ExpenseItem>
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}></ExpenseItem>
       <ExpenseItem
-        title={prop.expenses[3].title}
-        amount={prop.expenses[3].amount}
-        date={prop.expenses[3].date}></ExpenseItem>
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}></ExpenseItem>
     </div>
     
     
