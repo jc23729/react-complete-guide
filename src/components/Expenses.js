e62daa70057fb8319ad78859d8b9697213a32fe3
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-18 15:09:04 -0400

    update Expense.js
    moved expense items into file

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,28 @@
-function expenses()=>{
+import ExpenseItem from './ExpenseItem';
 
+function Expenses(props) {}=>{
+    return <div>
+    <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}></ExpenseItem>
+      <ExpenseItem
+        title={prop.expenses[1].title}
+        amount={prop.expenses[1].amount}
+        date={prop.expenses[1].date}></ExpenseItem>
+      <ExpenseItem
+        title={prop.expenses[2].title}
+        amount={prop.expenses[2].amount}
+        date={prop.expenses[2].date}></ExpenseItem>
+      <ExpenseItem
+        title={prop.expenses[3].title}
+        amount={prop.expenses[3].amount}
+        date={prop.expenses[3].date}></ExpenseItem>
+    </div>
+    
+    
+    
+    </div>
 }
+
+export default Expenses;
