43a722d4bec2c4fd60da642b7fc49323cca5572f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-18 12:52:51 -0400

    update

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -3,24 +3,16 @@
 //then import in the file you want to use it so you can use it
 //like an html element
 //main thing is to have one root element, so one <div></div> wrapped
+import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 294.67;
-  // props.title key has to match <ExpenseItem title={expenses[0].title}
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
+  // props.title key has to match <ExpenseItem title={expenses[0].title
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
